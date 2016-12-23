d457dd799c6d3b9cddd11983a1006bdf03dce1da
Shekhar Karande <shekhar.karande@gamesys.co.uk>
2016-12-23 20:35:02 +0000

    committing the first set of practice problems.

--- 02-functions/notes.js
+++ 02-functions/notes.js
@@ -1,2 +1,9 @@
 // your notes here!
 console.log("hello world!");
+console.log("some random string to test if i remember the code");
+
+function greet(name){
+	return "hello " + name;
+};
+
+
--- 02-functions/practice.js
+++ 02-functions/practice.js
@@ -1,6 +1,7 @@
 // (example) Write a function that accepts two number arguments and adds them
 // together.
-var add = function () {
+var add = function (numFirst, numSecond) {
+	return numFirst + numSecond;
 };
 
 
@@ -9,7 +10,8 @@ var add = function () {
 //
 //     var preTaxTotal = totalCost(5, 5.99); // 5 items at 5.99
 //     //=> 29.950000000000003
-var totalCost = function () {
+var totalCost = function (cost, quantity) {
+	return cost * quantity;
 };
 
 
@@ -21,7 +23,8 @@ var totalCost = function () {
 //
 //     cardString("queen", "hearts");
 //     //=> queen of hearts
-var cardString = function () {
+var cardString = function (cardRank, cardSuit) {
+	return cardRank + " of " + cardSuit;
 };
 
 
@@ -33,7 +36,8 @@ var cardString = function () {
 //
 //     openTag("div");
 //     //=> <div>
-var openTag = function () {
+var openTag = function (tagName) {
+	return "<" + tagName + ">";
 };
 
 
@@ -45,7 +49,8 @@ var openTag = function () {
 //
 //     closeTag("div");
 //     //=> </div>
-var closeTag = function () {
+var closeTag = function (tagName) {
+	return "</"+tagName+">";
 };
 
 
@@ -62,7 +67,8 @@ var closeTag = function () {
 //
 // Although I won't be testing for this, your code should re-use the functions that
 // you created in the previous section.
-var toTagString = function () {
+var toTagString = function (tagName,tagContent) {
+	return openTag(tagName) + tagContent + closeTag(tagName);
 };
 
 
